b6fa8771ed615e74903208073933e43ee953d3e7
katalystkat <kattsaib@gmail.com>
2024-02-03 22:18:48 +0800

    removed personal info

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -10,48 +10,49 @@ export async function GET(req: NextRequest) {
     new URL('../../public/fonts/kaisei-tokumin-bold.ttf', import.meta.url)
   ).then((res) => res.arrayBuffer());
   const fontData = await font;
-
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          height: '100%',
-          width: '100%',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'flex-start',
-          justifyContent: 'center',
-          backgroundImage: 'url(https://leerob.io/og-bg.png)',
-        }}
-      >
-        <div
-          style={{
-            marginLeft: 190,
-            marginRight: 190,
-            display: 'flex',
-            fontSize: 130,
-            fontFamily: 'Kaisei Tokumin',
-            letterSpacing: '-0.05em',
-            fontStyle: 'normal',
-            color: 'white',
-            lineHeight: '120px',
-            whiteSpace: 'pre-wrap',
-          }}
-        >
-          {postTitle}
-        </div>
-      </div>
-    ),
-    {
-      width: 1920,
-      height: 1080,
-      fonts: [
-        {
-          name: 'Kaisei Tokumin',
-          data: fontData,
-          style: 'normal',
-        },
-      ],
-    }
-  );
+  return 
+  // Commented out below to reduce deployment size, edge function limited at 1MB
+  // return new ImageResponse(
+  //   (
+  //     <div
+  //       style={{
+  //         height: '100%',
+  //         width: '100%',
+  //         display: 'flex',
+  //         flexDirection: 'column',
+  //         alignItems: 'flex-start',
+  //         justifyContent: 'center',
+  //         backgroundImage: 'url(https://leerob.io/og-bg.png)',
+  //       }}
+  //     >
+  //       <div
+  //         style={{
+  //           marginLeft: 190,
+  //           marginRight: 190,
+  //           display: 'flex',
+  //           fontSize: 130,
+  //           fontFamily: 'Kaisei Tokumin',
+  //           letterSpacing: '-0.05em',
+  //           fontStyle: 'normal',
+  //           color: 'white',
+  //           lineHeight: '120px',
+  //           whiteSpace: 'pre-wrap',
+  //         }}
+  //       >
+  //         {postTitle}
+  //       </div>
+  //     </div>
+  //   ),
+  //   {
+  //     width: 1920,
+  //     height: 1080,
+  //     fonts: [
+  //       {
+  //         name: 'Kaisei Tokumin',
+  //         data: fontData,
+  //         style: 'normal',
+  //       },
+  //     ],
+  //   }
+  // );
 }
